116c1a349c29312debcb8a27e1812cc9ff50ab21
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-07-07 22:08:41 +0300

    array map

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+// Assuming tutorials is a global variable containing the tutorial names
+
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    // Split the tutorial name into an array of words
+    var words = tutorial.split(' ');
+  
+    // Capitalize the first letter of each word and convert the rest to lowercase
+    var titleCaseWords = words.map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+  
+    // Join the title-cased words back into a single string
+    var titleCasedTutorial = titleCaseWords.join(' ');
+  
+    return titleCasedTutorial;
+  });
+}
+
